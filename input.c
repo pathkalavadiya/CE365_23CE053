320c46cacde5d8f6fe30559a0a92facdc15a8d62
pathkalavadiya <pathkalavadiya1902@gmail.com>
2026-02-01 22:06:47 +0530

    input file for practical 3

---
+++ input.c
@@ -0,0 +1,43 @@
+// int main() {
+//     int a = 5 , 7H;
+//     // assign value
+//     char b = 'x';
+//     /* return
+//        value */
+//     return a + b;
+// }
+/* salary calculation */
+/* salary calculation*/
+
+
+// void main()
+// {
+// long int bs , da , hra , gs;
+// scanf("%ld",&bs);
+// da=bs*.40;
+// hra=bs*.20;
+// gs=bs+da+hra;
+// }
+
+// struct student
+// {
+// int id;
+// float cgpa;
+// };
+// void main()
+// {
+// student s;
+// s.id = 10;
+// s.cgpa = 8.7;
+// }
+
+void add ( int , int );
+void main()
+{
+int a , b;
+add ( a , b );
+}
+void add ( int x , int y )
+{
+return x + y;
+}
